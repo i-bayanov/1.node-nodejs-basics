5ee92d5076bf8e60ec34baafb490a3bfe12ae90a
Ilia Bayanov <i.bajanov@itorum.ru>
2023-06-18 23:04:23 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+import { defineDirAndFilenameConstants, checkPathExistence } from '../misc/index.js';
+
+const { __dirname } = defineDirAndFilenameConstants(import.meta.url);
+const WRONG_FILENAME = path.resolve(__dirname, 'files', 'wrongFilename.txt');
+const PROPER_FILENAME = path.resolve(__dirname, 'files', 'properFilename.md');
+
 const rename = async () => {
-    // Write your code here 
+  const isWrongFileExists = await checkPathExistence(WRONG_FILENAME);
+  const isProperFileExists = await checkPathExistence(PROPER_FILENAME);
+
+  if (!isWrongFileExists || isProperFileExists) throw new Error('FS operation failed');
+
+  await fsPromises.rename(WRONG_FILENAME, PROPER_FILENAME);
 };
 
 await rename();
