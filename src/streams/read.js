313d9de3f60ca6b755cb57b3bf6e93753079af11
Ilia Bayanov <i.bajanov@itorum.ru>
2023-06-19 00:20:53 +0300

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import path from 'path';
+
+import { defineDirAndFilenameConstants } from '../misc/index.js';
+
+const { __dirname } = defineDirAndFilenameConstants(import.meta.url);
+const FILENAME_TO_READ = path.resolve(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  createReadStream(FILENAME_TO_READ).pipe(stdout);
 };
 
 await read();
