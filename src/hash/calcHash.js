d2ac4d6c4b17b89f86444f65521046003dac0121
Ilia Bayanov <i.bajanov@itorum.ru>
2023-06-19 00:06:28 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,30 @@
+import { createReadStream } from 'fs';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { createHash } from 'crypto';
+
+import { defineDirAndFilenameConstants } from '../misc/index.js';
+
+const { __dirname } = defineDirAndFilenameConstants(import.meta.url);
+const FILENAME_TO_HASH = path.resolve(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileData = await readFile(FILENAME_TO_HASH);
+  const hash = createHash('sha256').update(fileData).digest('hex');
+
+  console.log(hash);
+
+  // Or the same functionality with streams API
+  // const hash = createHash('sha256');
+  // const readStream = createReadStream(FILENAME_TO_HASH);
+  // readStream.on('readable', () => {
+  //   const data = readStream.read();
+  //   if (data) {
+  //     hash.update(data);
+  //   } else {
+  //     console.log(hash.digest('hex'));
+  //   }
+  // });
 };
 
 await calculateHash();
