e42922a11fa281954d9c8ec58ecc29768492a717
Ilia Bayanov <i.bajanov@itorum.ru>
2023-06-19 00:22:45 +0300

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import path from 'path';
+
+import { defineDirAndFilenameConstants } from '../misc/index.js';
+
+const { __dirname } = defineDirAndFilenameConstants(import.meta.url);
+
+const FILENAME_TO_WRITE = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  stdin.pipe(createWriteStream(FILENAME_TO_WRITE));
 };
 
 await write();
